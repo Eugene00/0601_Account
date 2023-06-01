8443bf7b6b6db53d5c2724988e7e92da7bb8790c
kirim <kirim.park@naver.com>
2023-06-01 09:54:54 +0900

    getZegop implementation

--- Cal-Test/test.cpp
+++ Cal-Test/test.cpp
@@ -5,3 +5,8 @@ TEST(TestCaseName, TestName) {
 	Cal* cal = new Cal();
 	EXPECT_TRUE(cal != nullptr);
 }
+
+TEST(TestCaseName, ZegopTest) {
+	Cal* cal = new Cal();
+	EXPECT_EQ(49, cal->getZegop(7));
+}

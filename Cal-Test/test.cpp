ae9c5f8657134318d19bbacf54ab8b835286d788
Seokhwan <sukkakim@gmail.com>
2023-06-01 09:58:59 +0900

    Add getSum()

--- Cal-Test/test.cpp
+++ Cal-Test/test.cpp
@@ -9,9 +9,16 @@ TEST(TestCaseName, TestName) {
 TEST(TestCaseName, ZegopTest) {
 	Cal* cal = new Cal();
 	EXPECT_EQ(49, cal->getZegop(7));
+}
 
 TEST(TestCaseName, getGop01) {
 	EXPECT_EQ(Cal().getGop(2, 3), 6);
 	EXPECT_EQ(Cal().getGop(5, 10), 50);
 	EXPECT_EQ(Cal().getGop(9, 9), 81);
+}
+
+TEST(TestCaseName, getSum) {
+	int a = 1, b = 2;
+	Cal* cal = new Cal();
+	EXPECT_TRUE(3 == cal->getSum(a, b));
 }
--- Cal/cal.cpp
+++ Cal/cal.cpp
@@ -1,13 +1,18 @@
 class Cal
 {
 public:
-
 	int getZegop(int a)
 	{
 		return a * a;
+	}
 
 	int getGop(int a, int b)
 	{
 		return a * b;
+	}
+
+	int getSum(int a, int b)
+	{
+		return a + b;
 	}
 };
